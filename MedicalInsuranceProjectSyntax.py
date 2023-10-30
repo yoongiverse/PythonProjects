9b6c393bbf41c3c16ef06641c243ebae99a1c8d4
yoongiverse <99763422+yoongiverse@users.noreply.github.com>
2023-10-29 23:36:52 -0600

    Create MedicalInsuranceProjectSyntax.py

---
+++ MedicalInsuranceProjectSyntax.py
@@ -0,0 +1,32 @@
+# create the initial variables below
+age = 28
+sex=0 #0 for female and 1 for male
+bmi=26.2
+num_of_children=3
+smoker=0
+
+# Add insurance estimate formula below
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
+
+# Age Factor
+age += 4
+
+#recalculate insurance cost
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated insurance cost after increasing the age by 4 years is " + str(change_in_insurance_cost) + " dollars")
+
+# BMI Factor and how a change affects insurance costs
+bmi+=3.1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated insurance cost after increasing BMI by 3.1 is " + str(change_in_insurance_cost) + "dollars")
+
+
